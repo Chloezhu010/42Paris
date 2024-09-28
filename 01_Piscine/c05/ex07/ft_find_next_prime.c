e170209411fcc08e478e14bd7046a4f3d28f58ee
chloezhu010 <chloe.zhu.x@gmail.com>
2024-09-28 22:28:58 +0200

    complete c05

--- 01_Piscine/c05/ex07/ft_find_next_prime.c
+++ 01_Piscine/c05/ex07/ft_find_next_prime.c
@@ -1,15 +1,31 @@
 int	ft_find_prime(int nb)
-
+{
+	int i;
+	
+	i = 2;
+	if (nb <= 1)
+		return (1);
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	ft_find_next_prime(int nb)
 {
-	int	i;
-
-	
+	if (nb <= 2)
+		return (2);
+	while (ft_find_prime(nb) == 0)
+		nb++;
+	return (nb);	
 }
 
 #include <stdio.h>
 int	main(void)
 {
 	printf("%d\n", ft_find_next_prime(5));
+        printf("%d\n", ft_find_next_prime(24));
 }
