82cf3fa60e814e17d06bd8e5d340bee9ed20cda5
chloezhu010 <chloe.zhu.x@gmail.com>
2024-09-29 21:41:09 +0200

    update ex03

--- 01_Piscine/c06/ex03/ft_sort_params.c
+++ 01_Piscine/c06/ex03/ft_sort_params.c
@@ -1,4 +1,4 @@
-#include <unistdi.h>
+#include <unistd.h>
 
 void	ft_putstr(char *str)
 {
@@ -17,12 +17,21 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] || s2[i] && (s1[i] == s2[i]))
+	while ((s1[i] || s2[i]) && (s1[i] == s2[i]))
 		i++;	
 	return (s1[i] - s2[i]);
 }
 
-void	ft_sort_str(char)
+void	ft_sort_str(char **s1, char **s2)
+{
+	char	*temp;
+	if (ft_strcmp(*s1, *s2) > 0)
+	{
+		temp = *s1;
+		*s1 = *s2;
+		*s2 = temp;
+	}
+}
 
 int	main(int argc, char *argv[])
 {
@@ -30,10 +39,22 @@ int	main(int argc, char *argv[])
 	int	j;
 
 	i = 1;
-	j = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				ft_sort_str(&argv[i], &argv[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
 	while (i < argc)
 	{
-		while (j < argc)
-			if (ft_cal_ascii(str[j]) > i	
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
 	}
 }
