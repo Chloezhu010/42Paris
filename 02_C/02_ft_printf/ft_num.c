819d6c1395e5d74543b9274027b75e3695268a76
chloezhu010 <chloe.zhu.x@gmail.com>
2024-12-08 14:36:22 +0100

    add main to test

--- 02_C/02_ft_printf/ft_num.c
+++ 02_C/02_ft_printf/ft_num.c
@@ -45,7 +45,7 @@ int	print_unsigned(unsigned int n)
 		count = print_unsigned(n / 10);
 		return (count + print_unsigned(n % 10)); 
 	}
-	return (print_unsigned(n + '0'));
+	return (print_char(n + '0'));
 }
 
 
@@ -91,3 +91,56 @@ int	print_ptr(size_t ptr)
 		return (count + write(1, "0", 1));
 	return (count + print_ptr_hex(ptr));	
 }
+/*
+#include <limits.h>
+#include <stdio.h>
+int	main()
+{
+	// test on %d & %i
+	print_digit(0);
+	print_char('\n');
+	print_digit(42);
+	print_char('\n');
+	print_digit(-42);
+	print_char('\n');
+	print_digit(INT32_MIN);
+	print_char('\n');
+	print_digit(INT32_MAX);
+	print_char('\n');
+	printf("%d\n %d\n %d\n %d\n %d\n ", 0, 42, -42, INT32_MIN, INT32_MAX);
+
+	// test on %u
+	print_unsigned(0);
+	print_char('\n');
+	print_unsigned(UINT_MAX);
+	print_char('\n');
+	printf("%u\n %u\n ", 0, UINT_MAX);
+
+	// test on %x and %X
+	print_hex(-10, 'x');
+	print_char('\n');
+	print_hex(0, 'x');
+	print_char('\n');
+	print_hex(UINT_MAX, 'X');
+	print_char('\n');
+	printf("%x\n %x\n %X\n ", -10, 0, UINT_MAX);
+
+	// test on %p
+	int	num = 42;
+	char str[] = "hello world";
+	void *ptr1 = &num;
+	void *ptr2 = str;
+	void *ptr3 = NULL;
+	
+	int len1_real = printf("%p", ptr1);
+	int len1_my = print_ptr((size_t)ptr1);
+	printf("\nreal vs my: %d : %d \n", len1_real, len1_my);
+
+	int len2_real = printf("%p", ptr2);
+	int len2_my = print_ptr((size_t)ptr2);
+	printf("\nreal vs my: %d : %d \n", len2_real, len2_my);
+
+	int len3_real = printf("%p", ptr3);
+	int len3_my = print_ptr((size_t)ptr3);
+	printf("\nreal vs my: %d : %d \n", len3_real, len3_my);
+}*/
