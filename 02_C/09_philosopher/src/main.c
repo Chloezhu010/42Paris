6f70c3d7eac2b038d6afbf9d2123022ca8e3a154
chloezhu010 <chloe.zhu.x@gmail.com>
2025-02-03 13:39:24 +0100

    update parsing argument

--- 02_C/09_philosopher/src/main.c
+++ 02_C/09_philosopher/src/main.c
@@ -1,1 +1,18 @@
 #include "../incl/philo.h"
+
+int main(int ac, char **av)
+{
+    t_data  data;
+
+    // parse command-line arg
+    data = parse_argument(ac, av);
+    
+    // initialize shared data (forks, philo states etc.)
+
+    // create philo threads
+
+    // monitor the simulation (check for philo death or completion)
+
+    // cleanup resource (destroy mutex, free memeory etc.)
+
+}
---
+++ 02_C/09_philosopher/src/utils.c
@@ -0,0 +1,26 @@
+#include "../incl/philo.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	negative;
+	int	i;
+	int	result;
+
+	i = 0;
+	negative = 1;
+	result = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			negative = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (result * negative);
+}
